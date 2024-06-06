b8b61047892bdbb93e7ef2b818bdcb26d198dae9
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-06 15:53:08 +0200

    lettura e scrittura su file in modo sync

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const saluta = require('./utils');
-const nomi = require('./nomi');
 
-const os = require("os");
-console.log(os.userInfo());
-console.log(os.uptime());
 
-const path = require("path");
-console.log(path.sep);
-console.log(__dirname);
+const {writeFileSync, readFileSync} = require ("fs");
 
+const prova = readFileSync('./cartella/prova.txt','utf8');
+const ciao = readFileSync('./cartella/ciao.txt','utf8');
+
+console.log(prova);
+
+//se metto 'a' mette append, se do un path inesistent lo crea
+writeFileSync('./cartella/prova.txt', 'prova ciaoooo', {flag: 'a'}); 
