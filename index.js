b46fc9d1148f59bd53f02ba8ba5f5ad96fb59995
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-06 15:46:01 +0200

    Esempio di moduli built-in (os e path)

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const saluta = require('./utils');
 const nomi = require('./nomi');
 
-saluta(nomi.persona1);
-saluta("anna");
-saluta(nomi.persona2);
+const os = require("os");
+console.log(os.userInfo());
+console.log(os.uptime());
+
+const path = require("path");
+console.log(path.sep);
+console.log(__dirname);
+
