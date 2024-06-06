a23dc8a74e080088064f7151c3fa67f1073238cb
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-06 16:03:04 +0200

    esempio lettura e scrittura file in modo async

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/* 
 
 const {writeFileSync, readFileSync} = require ("fs");
 
@@ -8,4 +8,30 @@ const ciao = readFileSync('./cartella/ciao.txt','utf8');
 console.log(prova);
 
 //se metto 'a' mette append, se do un path inesistent lo crea
-writeFileSync('./cartella/prova.txt', 'prova ciaoooo', {flag: 'a'}); 
+writeFileSync('./cartella/prova.txt', 'prova ciaoooo', {flag: 'a'});  */
+
+
+const {writeFile, readFile} = require ("fs");
+readFile('./cartella/prova.txt','utf8', (error,result)=>{
+    if(error){
+        console.log(error);
+        return;
+    }
+    const prova = result;
+    console.log(prova);
+    readFile('./cartella/ciao.txt','utf8', (error,result)=>{
+        if(error){
+            console.log(error);
+            return;
+        }
+        const ciao = result;
+        console.log(ciao);
+        writeFile('./cartella/3file.txt','ciaooaaoaoaa oooo', (error,result)=>{
+            if(error){
+                console.log(error);
+                return;
+            }
+            console.log(result);
+        });
+    });
+});
