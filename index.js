92061d2a0e82e8a322d5d8808b2eacf21dfdf926
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-06 17:55:27 +0200

    esempio di Event emitters

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("ok");
+const EventEmitter = require('events');
+const customEmitter = new EventEmitter();
 
+customEmitter.on('messaggio', (nome,anno)=>{
+    console.log("sono partito, evento manda messaggio "+nome+" e "+anno);
+})
+
+customEmitter.on('messaggio', (nome)=>{
+    console.log("evento, manda notifica "+nome);
+})
+
+customEmitter.on('messaggio', ()=>{
+    console.log("evento vuoto");
+})
+
+customEmitter.emit("messaggio", "ionut", 2024);
