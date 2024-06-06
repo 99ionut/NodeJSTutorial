bc130295bab758b0bfb9cd4a3eefb6c22b5c8c99
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-06 16:46:53 +0200

    HTTP server con routing

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
-/* 
+const http = require('http');
 
-const {writeFileSync, readFileSync} = require ("fs");
-
-const prova = readFileSync('./cartella/prova.txt','utf8');
-const ciao = readFileSync('./cartella/ciao.txt','utf8');
-
-console.log(prova);
-
-//se metto 'a' mette append, se do un path inesistent lo crea
-writeFileSync('./cartella/prova.txt', 'prova ciaoooo', {flag: 'a'});  */
-
-
-const {writeFile, readFile} = require ("fs");
-readFile('./cartella/prova.txt','utf8', (error,result)=>{
-    if(error){
-        console.log(error);
-        return;
+const server = http.createServer((req,res)=>{
+    if(req.url === "/"){
+        res.end("Benvenuto sul sito");
+    }else if(req.url === "/ionut"){
+        res.end("il profilo di ionut");
+    } else {
+        res.end(`<h1>errore</h1>`);
     }
-    const prova = result;
-    console.log(prova);
-    readFile('./cartella/ciao.txt','utf8', (error,result)=>{
-        if(error){
-            console.log(error);
-            return;
-        }
-        const ciao = result;
-        console.log(ciao);
-        writeFile('./cartella/3file.txt','ciaooaaoaoaa oooo', (error,result)=>{
-            if(error){
-                console.log(error);
-                return;
-            }
-            console.log(result);
-        });
-    });
-});
+})
+
+server.listen(3000);
+
