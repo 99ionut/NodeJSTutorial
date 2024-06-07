30d710e70ed4270610fdcd7bad99e90116eb585e
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-07 11:30:48 +0200

    richieste con postman

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ const express = require('express')
 const app = express()
 const {persone} = require('./persone');
 
-//middleware = funzione eseguita durante la richiesta req -> middleware -> res
-const middlewareProva = require('./middlewareProva');
-app.use(middlewareProva)
-//per usarlo solo su alcuni percorsi (es tutto quello che viene dopo /persone app.use("/persone",middlewareProva)
-//per usare piu middleware app.use([middlewareProva1, middlewareProva2 ecc...])
-
 app.get("/", (req, res) =>{
     res.send("<h1>homepage</h1>");
 })
 
-//app.get("/about", middlewareProva, (req, res) =>{ oppure app.use(middlewareProva) per usarlo su tutte le funzioni
-app.get("/about", (req, res) =>{
-    res.send("<h1>about</h1>");
+app.post("/", (req, res) =>{
+    res.send("<h1>post homepage</h1>");
+})
+
+app.put("/", (req, res) =>{
+    res.send("<h1>put homepage</h1>");
+})
+
+app.delete("/", (req, res) =>{
+    res.send("<h1>delete homepage</h1>");
+})
+
+app.get("/json", (req, res) =>{
+    res.json(persone);
+})
+
+app.get("/area", (req, res) =>{
+    res.send(req.body);
 })
 
 app.listen(3000);
--- middlewareProva.js
+++
@@ -1,9 +0,0 @@
-const middlewareProva = (req, res, next) =>{
-    const {method, url} = req
-    const time = new Date().getMinutes()
-    console.log(method, url, time)
-    //fa proseguire la richiesta verso il resto della funione, nel nostro caso res.send
-    next()
-}
-
-module.exports = middlewareProva;
