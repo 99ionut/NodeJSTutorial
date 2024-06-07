dd34a2c20aeca9e172b7445a1f54c8df5e2a49b9
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-07 12:13:04 +0200

    Esempio di API

--- index.js
+++ index.js
@@ -2,28 +2,38 @@ const express = require('express')
 const app = express()
 const {persone} = require('./persone');
 
-app.get("/", (req, res) =>{
-    res.send("<h1>homepage</h1>");
+app.use(express.json());
+
+app.get("/api/persone", (req, res) =>{
+    res.status(200).json({data:persone});
 })
 
-app.post("/", (req, res) =>{
-    res.send("<h1>post homepage</h1>");
+app.get("/api/persone/:id", (req, res) =>{
+    const {id} = req.params;
+    const persona = persone.find((persona=>persona.id===id))
+    res.json(persona);
+});
+
+app.post("/api/persone", (req, res) =>{
+    console.log(req.body);
+    const persona = req.body;
+    persone.push(persona);
+    res.status(200).json({success:true, data:persone});
 })
 
-app.put("/", (req, res) =>{
-    res.send("<h1>put homepage</h1>");
+app.put("/api/persone/:id", (req, res) =>{
+    const {id} = req.params;
+    const persona = req.body;
+    persone[Number(id)-1] = persona;
+    res.status(200).json({success: true})
 })
 
-app.delete("/", (req, res) =>{
-    res.send("<h1>delete homepage</h1>");
+app.delete("/api/persone/:id", (req, res) =>{
+    const {id} = req.params;
+    const index =  persone.findIndex(persona => persona.id === id);
+    persone.splice(index,1);
+    res.status(200).json({success: true})
 })
 
-app.get("/json", (req, res) =>{
-    res.json(persone);
-})
-
-app.get("/area", (req, res) =>{
-    res.send(req.body);
-})
 
 app.listen(3000);
