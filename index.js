75f51df5ab72a90d11fc9d4c45bbca262b65f66c
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-07 10:06:34 +0200

    Routing con parametri

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
-const persone = require('./persone');
+const {persone} = require('./persone');
 
 app.get("/", (req, res) =>{
-    res.json(persone);
+    res.send("<h1>homepage</h1>");
 })
+
+
+app.get("/persone", (req, res) =>{
+    const nuovaPersona = persone.map((persona)=>{
+        const {nome,cognome} = persona;
+        return {nome, cognome}
+    })
+    res.json(nuovaPersona);
+})
+
+app.get("/persone/:id", (req, res)=>{
+    console.log(req.params);
+    const id = req.params.id;
+    const persona = persone.find((persona)=> persona.id === id);
+
+    if(!persona){
+        return res.status(404).json({message: "non trovato"});
+    }
+    res.json(persona);
+});
 
 app.listen(3000);
--- persone.js
+++ persone.js
@@ -1,8 +1,8 @@
 const persone = [
-    {nome: "ionut", cognome: "rossi", eta:20}, 
-    {nome: "giulia", cognome: "neri", eta:21},
-    {nome: "aaaa", cognome: "AAAA", eta:22}, 
-    {nome: "bbbb", cognome: "BBBB", eta:23}
+    {id:"1", nome: "ionut", cognome: "rossi", eta:20}, 
+    {id:"2", nome: "giulia", cognome: "neri", eta:21},
+    {id:"3", nome: "aaaa", cognome: "AAAA", eta:22}, 
+    {id:"4", nome: "bbbb", cognome: "BBBB", eta:23}
 ]
 
 module.exports = {persone}
