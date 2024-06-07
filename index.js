c2217a07dd9c75f784f1f02eb4a0226c4962b4a4
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-07 10:51:40 +0200

    Gestione Query String

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.get("/persone", (req, res) =>{
     res.json(nuovaPersona);
 })
 
-app.get("/persone/:id", (req, res)=>{
+//questi parametri devono essere gestiti
+/* app.get("/persone/:id", (req, res)=>{
     console.log(req.params);
     const id = req.params.id;
     const persona = persone.find((persona)=> persona.id === id);
@@ -24,6 +25,30 @@ app.get("/persone/:id", (req, res)=>{
         return res.status(404).json({message: "non trovato"});
     }
     res.json(persona);
+}); */
+
+//questi non sono obbligatori
+//filtra per iniziale
+//http://localhost:3000/persone/search?query=a&limit=1
+app.get("/persone/search", (req, res)=>{
+    console.log(req.query);
+    const {query, limit} = req.query;
+    let personeFiltrate = [...persone];
+    if(query){
+        personeFiltrate = personeFiltrate.filter((persona)=>{
+            return persona.nome.startsWith(query);
+        })
+    }
+
+    if(limit){
+        personeFiltrate = personeFiltrate.slice(0,Number(limit));
+    }
+
+    if(personeFiltrate.length < 1){
+        return res.status(200).json({success:true, messaggio:"nessun dato"})
+    }
+
+    res.status(200).json(personeFiltrate);
 });
 
 app.listen(3000);
--- persone.js
+++ persone.js
@@ -1,8 +1,11 @@
 const persone = [
     {id:"1", nome: "ionut", cognome: "rossi", eta:20}, 
     {id:"2", nome: "giulia", cognome: "neri", eta:21},
-    {id:"3", nome: "aaaa", cognome: "AAAA", eta:22}, 
-    {id:"4", nome: "bbbb", cognome: "BBBB", eta:23}
+    {id:"3", nome: "aaaa", cognome: "AAAA1", eta:22}, 
+    {id:"4", nome: "bbbb", cognome: "BBBB1", eta:23},
+    {id:"5", nome: "aaaa", cognome: "AAAA2", eta:22}, 
+    {id:"6", nome: "bbbb", cognome: "BBBB2", eta:23},
+    {id:"7", nome: "aaaa", cognome: "AAAA3", eta:255},
 ]
 
 module.exports = {persone}
