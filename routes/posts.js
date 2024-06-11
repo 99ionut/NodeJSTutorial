30505293b411683e4e2cd27811aafca0c9a3b468
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-11 14:41:46 +0200

    Put and Delete example

--- routes/posts.js
+++ routes/posts.js
@@ -47,4 +47,31 @@ router.post("/", (req,res)=>{
     res.status(201).json(posts);
 });
 
+//put
+router.put("/:id", (req,res)=>{
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+        return res.status(404).json({msg: "no id found"});
+    }
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+});
+
+
+//delete
+router.delete("/:id", (req,res)=>{
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+        return res.status(404).json({msg: "no id found"});
+    }
+
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
+});
+
 module.exports = router;
